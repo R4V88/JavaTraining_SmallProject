3ae1c1a6b636b844f980b5ed135b1c46a5baad3d
R4v88 <sochacki.rfl@gmail.com>
2020-04-06 18:33:28 +0200

    feat: UserValidator class is ready

--- src/main/java/validator/UserValidator.java
+++ src/main/java/validator/UserValidator.java
@@ -1,4 +1,52 @@
 package validator;
 
+import api.UserService;
+import exception.UserLoginAlreadyExistException;
+import exception.UserShortLengthLoginException;
+import exception.UserShortLengthPasswordException;
+import model.User;
+
+import java.util.List;
+
 public class UserValidator {
+
+    private UserService userService;
+
+    private static UserValidator instance = null;
+
+    public static UserValidator getInstance() {
+        if(instance == null) {
+            instance = new UserValidator();
+        }
+        return instance;
+    }
+
+    public boolean isValidate(User user) throws UserShortLengthPasswordException, UserShortLengthLoginException, UserLoginAlreadyExistException {
+        return isPasswordValid(user) && isLoginValid(user) && isLoginExist(user);
+    }
+
+    private boolean isPasswordValid(User user) throws UserShortLengthPasswordException {
+        if(user.getPassword().length() < 6){
+            return true;
+        }
+        throw new UserShortLengthPasswordException("Password is too short!");
+    }
+
+    private boolean isLoginValid(User user) throws UserShortLengthLoginException {
+        if(user.getLogin().length() < 4){
+            return true;
+        }
+        throw new UserShortLengthLoginException("Login " + user.getLogin() + " is too short!");
+    }
+
+    private boolean isLoginExist(User user) throws UserLoginAlreadyExistException {
+        List<User> userList = userService.getAllUsers();
+        for(User userFromList : userList){
+            if(userFromList.getLogin().equals(user.getLogin())){
+                throw new UserLoginAlreadyExistException("User *" + user.getLogin() + "* already exist!");
+            }
+        }
+        return true;
+    }
+
 }
