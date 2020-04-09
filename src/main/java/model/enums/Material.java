39c2a933105c08c094358d5861a809e03440d4a2
R4v88 <sochacki.rfl@gmail.com>
2020-04-09 17:24:43 +0200

    feat: Enums - SkinType and Material added

---
+++ src/main/java/model/enums/Material.java
@@ -0,0 +1,5 @@
+package model.enums;
+
+public enum Material {
+    LEATHER, FUR, COTTON, WOOL, POLYESTER;
+}
---
+++ src/main/java/model/enums/SkinType.java
@@ -0,0 +1,5 @@
+package model.enums;
+
+public enum SkinType {
+    NATURAL, ARTIFICIAL;
+}
